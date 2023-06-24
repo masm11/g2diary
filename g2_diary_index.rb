b4bcf499ee09c0b7a18c42b3f13e6ed91d027a70
Yuuki Harano <masm+github@masm11.me>
2023-06-25 01:02:51 +0900

    normalize

--- g2_diary_index.rb
+++ g2_diary_index.rb
@@ -27,19 +27,36 @@ class G2DiaryIndex
     File.rename(new_path, INDEX_FILE)
   end
 
+  def normalize(str)
+    str
+      # 全角英数記号 http://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/uff00.html
+      .tr("\u{ff01}-\u{ff5e}", '!-~')
+      # quote文字 https://0g0.org/category/2000-206F/1/
+      .tr("\u{2018}\u{2019}\u{201a}\u{201b}\u{2032}\u{2035}", "`',`'`")
+      .tr("\u{201c}\u{201d}\u{201e}\u{201f}\u{2033}\u{2036}", '"')
+      # 円マーク
+      .tr('￥', '¥')
+      # ハイフン文字 https://garabakos.sakura.ne.jp/Finfs/infs00976.htm
+      .tr("\u{2010}\u{2212}\u{2014}\u{2015}\u{30fc}", '-')
+      # 空白文字 https://blog1.mammb.com/entry/2021/11/11/090000
+      .tr("\u{00a0}\u{2000}-\u{200a}\u{202f}\u{205f}\u{3000}\u{200b}\u{feff}", ' ')
+      # downcase
+      .tr('A-Z', 'a-z')
+  end
+
   def string_to_bigram(str)
+    str = normalize(str)
+
     kws = {}
 
     str.each_char do |c|
       next if c =~ /\s/    # 空白文字だったら無視
-      c.downcase!
       kws[c] = true
     end
 
     (0...str.length-1).each do |i|
       cc = str[i, 2]
       next if cc =~ /\s/    # 空白文字を含んでたら無視
-      cc.downcase!
       kws[cc] = true
     end
 
@@ -73,10 +90,10 @@ class G2DiaryIndex
     kws.each do |kw, _|
       paths = paths.slice(*paths_of(kw).keys)
     end
-    str.downcase!
+    str = normalize(str)
     paths.select! do |path|
       data = File.read(path)
-      data.downcase!
+      data = normalize(data)
       data.include? str
     end
     paths
